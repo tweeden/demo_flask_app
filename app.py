fd5a51f55f6b8288c25dc38e9076fa1c4af6cd94
buildingtruebelief <buildingtruebelief@gmail.com>
2018-09-06 13:46:18 -0500

    added new pages

--- app.py
+++ app.py
@@ -1,11 +1,34 @@
-from flask import Flask
+from flask import Flask, render_template
+from data import Products
+
 app = Flask(__name__)
+Products = Products()
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def home():
+    return render_template('home.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
+@app.route('/products')
+def products():
+    return render_template('products.html', products=Products)
+
+
+# @app.route('/product/<string:id>/')
+# def product(id):
+#     return render_template('product.html', id=id)
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
---
+++ data.py
@@ -0,0 +1,26 @@
+def Products():
+    products = [
+        {
+            'id': 1,
+            'name': 'Item One',
+            'discription': 'Bacon ipsum dolor amet pork loin shank short loin, shankle ground round turkey ham hock venison beef ribs turducken shoulder chicken pork chop. Tail tri-tip beef ribs drumstick ribeye ham. Sausage shankle cupim prosciutto, pancetta pig tri-tip filet mignon. Cow andouille brisket swine flank tenderloin ham fatback turkey shank sirloin pork pig ground round. Pork belly kielbasa picanha cow.',
+            'price': 0.00
+
+        },
+        {
+            'id': 2,
+            'name': 'Item Two',
+            'discription': 'Bacon ipsum dolor amet pork loin shank short loin, shankle ground round turkey ham hock venison beef ribs turducken shoulder chicken pork chop. Tail tri-tip beef ribs drumstick ribeye ham. Sausage shankle cupim prosciutto, pancetta pig tri-tip filet mignon. Cow andouille brisket swine flank tenderloin ham fatback turkey shank sirloin pork pig ground round. Pork belly kielbasa picanha cow.',
+            'price': 0.00
+
+        },
+        {
+            'id': 3,
+            'name': 'Item Three',
+            'discription': 'Bacon ipsum dolor amet pork loin shank short loin, shankle ground round turkey ham hock venison beef ribs turducken shoulder chicken pork chop. Tail tri-tip beef ribs drumstick ribeye ham. Sausage shankle cupim prosciutto, pancetta pig tri-tip filet mignon. Cow andouille brisket swine flank tenderloin ham fatback turkey shank sirloin pork pig ground round. Pork belly kielbasa picanha cow.',
+            'price': 0.00
+
+        },
+    ]
+
+    return products
